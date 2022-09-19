cde7e12d7375b3bfe5596928493031be0b1906da
Taofeek <taofeekolamilekan218@gmail.com>
2022-09-19 19:51:29 +0100

    type added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd78da9cbdb2bdaf3acdf4a4954d17e6705f2f4ae',
+        'name' => 'wahabtaofeeqo/todo-api',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wahabtaofeeqo/todo-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd78da9cbdb2bdaf3acdf4a4954d17e6705f2f4ae',
+            'dev_requirement' => false,
+        ),
+    ),
+);
